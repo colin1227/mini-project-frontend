c685b2054c6913cfb4906016635a06099aae2cfc
Colin Daniel <colinmdaniel@gmail.com>
2018-11-19 17:49:05 -0600

    it works

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Form, Label, Button } from 'semantic-ui-react';
 import './style.css';
+import getCookie from 'js-cookie';
 
 class Login extends Component {
   constructor(){
@@ -18,23 +19,28 @@ class Login extends Component {
   }
   handleSubmit = async (e) => {
     e.preventDefault();
-
-    const loginResponse = await fetch('http://localhost:9000/auth', {
+    const csrfCookie = getCookie('csrftoken');
+    const loginResponse = await fetch('http://localhost:8000/auth/login/', {
       method: 'POST',
       credentials: 'include', // this sends our session cookie with our request
       body: JSON.stringify(this.state),
       headers: {
+        'X-CSRFToken': csrfCookie,
         'Content-Type': 'application/json'
       }
     });
 
+    console.log(loginResponse, 'log in response line 33')
     const parsedResponse = await loginResponse.json();
 
-    if(parsedResponse.data === 'login successful'){
+    if (parsedResponse.data === "You are logged in"){
       // change our component
-      console.log('succes login')
+      console.log('success login')
       // this automatically get passed to your component as a prop
       this.props.history.push('/movies');
+    } else {
+      console.log('unsuccesful login');
+      console.log(parsedResponse.data, 'what is this ');
     }
   }
   render(){
--- src/MovieContainer/index.js
+++ src/MovieContainer/index.js
@@ -3,6 +3,7 @@ import CreateMovie from '../CreateMovie';
 import MovieList from '../MovieList';
 import EditMovie from '../EditMovie';
 import { Grid } from 'semantic-ui-react';
+import getCookie from 'js-cookie';
 
 class MovieContainer extends Component {
   constructor(){
@@ -18,37 +19,79 @@ class MovieContainer extends Component {
       showEditModal: false
     }
   }
+
   getMovies = async () => {
     // Where We will make our fetch call to get all the movies
-
+    // Where We will make our fetch call to get all the movies
+    const csrfCookie = getCookie('csrftoken');
+    const movies = await fetch('http://localhost:8000/movies/', {
+      credentials: 'include',
+      headers: {
+        'X-CSRFToken': csrfCookie
+      }
+    });
+    const moviesParsedJSON = await movies.json();
+    return moviesParsedJSON
   }
+
   componentDidMount(){
     // get ALl the movies, on the intial load of the APP
-
     /// Where you call this.getMovies
+    this.getMovies().then((movies)=> {
+      if (movies.message === 'Must be logged in to see this data'){
+        alert('You must be logged in to view movies!!')
+      } else {
+        this.setState({
+          movies: movies.data
+        })
+      }
+    }).catch((err) => {
+      console.log(err, 'this is the error')
+    })
   }
+  
   addMovie = async (movie, e) => {
     // .bind arguments take presidence over every other argument
     e.preventDefault();
     console.log(movie);
-
-
-
+    try {
+      const csrfCookie = getCookie('csrftoken');
+      const createdMovie = await fetch('http://localhost:8000/movies/', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfCookie
+        }
+      });
+      const parsedResponse = await createdMovie.json();
+      this.setState({movies: [...this.state.movies, parsedResponse.data]})
 
     } catch(err){
       console.log('error')
       console.log(err)
     }
-    // request address will start with 'http://localhost:9000'
-    // Set up your post request with fetch, Maybe lookup how do i do post request with fetch,
-    // headers: {'Content-Type': 'application/json'}
-    // becuase after we create it, we want to add it to the movies array
   }
   deleteMovie = async (id) => {
+    const csrfCookie = getCookie('csrftoken');
+    console.log(typeof csrfCookie, csrfCookie, 'csrf token/cookie')
+    const deletedMovie = await fetch('http://localhost:8000/movies/' + id + '/', {
+      headers: {
+        'X-CSRFToken': csrfCookie,
+        'Content-Type': 'application/json'
+      },
+      method: 'DELETE',
+      credentials: 'include'                              
+    });    
 
+    //This is the parsed response from the back end
+    const deletedMovieParsed = await deletedMovie;
+    console.log(deletedMovieParsed, 'deleted parsed movie')
 
-
-      // Then make the delete request, then remove the movie from the state array using filter
+    // Then make the delete request, then remove the movie from the state array using filter
+    this.setState({
+      movies: this.state.movies.filter((movie) => movie.id !== id )})
   }
   handleEditChange = (e) => {
 
@@ -57,7 +100,7 @@ class MovieContainer extends Component {
         ...this.state.movieToEdit,
         [e.currentTarget.name]: e.currentTarget.value
       }
-    });
+    });    
 
 
     // movieToEdit: {
@@ -67,14 +110,38 @@ class MovieContainer extends Component {
     // }
   }
   closeAndEdit = async (e) => {
-    // Put request,
-
-    // If you feel up to make the modal (EditMovie Component) and show at the appropiate times
-
+    e.preventDefault()
+    try {
+      const csrfCookie = getCookie('csrftoken');
+      const editResponse = await fetch('http://localhost:8000/movies/' + this.state.movieToEdit.id + '/', {
+        method: 'PUT',
+        body: JSON.stringify({
+          title: this.state.movieToEdit.title,
+          description: parseInt(this.state.movieToEdit.description)
+        }),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfCookie
+        }
+      });
+      const editResponseParsed = await editResponse.json();
+      const newEditedMovie = this.state.movies.map((movie) => {
+        if(movie.id === editResponseParsed.data.id){
+          movie = editResponseParsed.data
+        }
+        return movie        
+      });
+      this.setState({
+        showEditModal: false,
+        movies: newEditedMovie
+      });            
+    } catch(err) {
+        console.log(err)      
+    }
   }
   openAndEdit = (movieFromTheList) => {
     console.log(movieFromTheList, ' movieToEdit  ');
-
 
     this.setState({
       showEditModal: true,
@@ -82,11 +149,6 @@ class MovieContainer extends Component {
         ...movieFromTheList
       }
     })
-
-    // movieToEdit = {
-    //   title: movieFromTheList.title,
-    //   description: movieFromTheList.description
-    // }
   }
   render(){
     console.log(this.state)
